040694d899e1c185599058bb3ba14646effb087b
Lagnesh Thakur <lagneshthakur@gmail.com>
2025-02-12 23:27:31 +0530

    Update main.py
    Adds help text on base route

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! I can help you in generating questions from your query.</p>"
 
 @app.route("/generate_question")
 def generate_question():
